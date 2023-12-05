5a7ca1db4631e0c96faaf0def26369540c90b62a
ScrambledFox <github@jorislodewijks.nl>
2023-12-05 01:01:14 +0100

    feat: quick entry  page

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -2,13 +2,25 @@
 import ToolBar from "@/components/tools";
 import WallToolDrawer from "@/components/tools/tool/wallTool/wallToolDrawer";
 import Version from "@/components/version";
+import { useWallToolState } from "@/state/editor/tools/wallToolState";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const Canvas = dynamic(() => import("@/components/canvas"), {
   ssr: false,
 });
 
 export default function Page() {
+  useEffect(() => {
+    const onKeyUp = (e: any) => {};
+
+    window.addEventListener("keydown", onKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyUp);
+    };
+  }, []);
+
   return (
     <div className="h-screen">
       <Canvas />
